3ceffbb8cc42e432e971d587d1c2a1cf79a1e919
erikahjung <erika.h.jung@gmail.com>
2023-06-27 17:09:32 -0700

    well-functioning timer in seconds

--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -2,30 +2,41 @@ import React, { useState, useEffect } from "react";
 
 export const Timer = () => {
   const [timer, setTimer] = useState(0);
-  let intervalID;
+  const [intervalID, setIntervalID] = useState(null);
 
-  useEffect(() => {
-    intervalID = setInterval(() => setTimer((sec) => sec + 1), 1000);
+  // useEffect(() => {
+  //   setIntervalID(setInterval(() => setTimer((sec) => sec + 1), 1000));
 
-    return () => {
-      //setInterval is cleared on component unmount
-      clearInterval(intervalID);
+  //   return () => {
+  //     //setInterval is cleared on component unmount
+  //     clearInterval(intervalID);
+  //   }
+  // }, []);
+
+  const startTimer = () => {
+    if (!intervalID) {
+      setIntervalID(setInterval(() => setTimer((sec) => sec + 1), 1000));
     }
-  }, [])
+  };
 
   const stopTimer = () => {
-    console.log('stop');
-    console.log(intervalID); //undefined
-
     clearInterval(intervalID);
-  }
+    setIntervalID(null);
+  };
+
+  const restartTimer = () => {
+    stopTimer();
+    setTimer(0);
+  };
 
   return (
     <>
       <div id='timer'>
         {timer}
       </div>
-      <button onClick={stopTimer}>STOP</button>
+      <button id="start-button" onClick={startTimer}>START</button>
+      <button id="stop-button" onClick={stopTimer}>STOP</button>
+      <button id="restart-button" onClick={restartTimer}>RESTART</button>
     </>
   )
 };
