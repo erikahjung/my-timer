c0fd7e80ad937ae013e647b84e5e7994c688c090
erikahjung <erika.h.jung@gmail.com>
2023-08-10 14:04:06 -0700

    another way to implement re-render count

--- client/components/Count.jsx
+++ client/components/Count.jsx
@@ -1,15 +1,16 @@
 import React, { memo, useRef, useEffect } from "react";
 
 function Count() {
+  console.log('Count rendering...');
   const count = useRef(0);
 
-  useEffect(() => {
-    count.current++;
-    console.log(count.current);
-  }, [count.current]);
+  // useEffect(() => {
+  //   count.current++;
+  //   console.log(count.current);
+  // }, [count.current]);
 
   return (
-    <p className='count'>Count: {count.current}</p>
+    <p className='count'>Count: {count.current++}</p>
   )
 }
 
--- client/components/MemoCount.jsx
+++ client/components/MemoCount.jsx
@@ -1,15 +1,16 @@
 import React, { memo, useRef, useEffect } from "react";
 
 function MemoCount() {
+  console.log('MemoCount rendering...');
   const count = useRef(0);
 
-  useEffect(() => {
-    count.current++;
-    console.log(count.current);
-  }, [count.current]);
+  // useEffect(() => {
+  //   count.current++;
+  //   console.log(count.current);
+  // }, [count.current]);
 
   return (
-    <p className='count'>MemoCount: {count.current}</p>
+    <p className='count'>MemoCount: {count.current++}</p>
   )
 }
 
